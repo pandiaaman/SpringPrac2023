4d973d28f17a68fdc64fee4f1e08892ebd7d37e4
Aman Pandia <pandiaaman16@gmail.com>
2023-10-23 18:03:45 +0530

    first commit

---
+++ SpringPrac2023/src/main/java/com/pandiaaman/SpringPrac2023/App.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.SpringPrac2023;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
